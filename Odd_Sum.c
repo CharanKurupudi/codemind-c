87d4b2752477ff123fdd45952563eac0b24b3cca
CharanKurupudi <147598632+CharanKurupudi@users.noreply.github.com>
2023-12-19 21:58:55 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             sum+=arr[i];         }     }     printf("%d",sum); }

--- Odd_Sum.c
+++ Odd_Sum.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0,arr[n];
+    int n,i,sum=0;
     scanf("%d",&n);
+    int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
@@ -11,7 +12,7 @@ int main()
     {
         if(arr[i]%2!=0)
         {
-            sum=sum+arr[i];
+            sum+=arr[i];
         }
     }
     printf("%d",sum);
