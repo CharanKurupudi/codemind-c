2d698187c2b0ee05e9841251aee08c605514b4f8
CharanKurupudi <147598632+CharanKurupudi@users.noreply.github.com>
2024-01-16 12:51:08 +0530

    #include<stdio.h> int main() {     int a,i,j,k;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=i;j<a;j++)         {             printf(" ");         }         for(k=1;k<=i;k++)         {             printf("%c ",i+64);         }         printf(" ");     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,k;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=i;j<a;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+    }
+}
