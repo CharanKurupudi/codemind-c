3e57adcd05b81d46163d4869c0b198572de35ae8
CharanKurupudi <147598632+CharanKurupudi@users.noreply.github.com>
2024-01-16 21:28:43 +0530

    #include<stdio.h> int main() {     int a,i,f=0;     scanf("%d",&a);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<a;i++)     {         if(i%2==0 && arr[i]%2!=0)         {             f=1;             break;         }     }     if(f==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,f=0;
+    scanf("%d",&a);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        if(i%2==0 && arr[i]%2!=0)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
